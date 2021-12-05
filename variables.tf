092447a19c06f5e67e5a01853edb6d89a31b9133
Anzif M Azeez <anzifmazeez@gmail.com>
2021-12-05 10:28:39 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+
+  description = "Amazon Default Region"    
+  default = "Enter required region here"
+}
+
+variable "project" {
+  default = "Enter project name here"
+}
+
+variable "vpc_cidr" { 
+  default = "Enter cidr subnet details here"
+}
